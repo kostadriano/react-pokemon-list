5a4a9f30c0e66d4e9e6999a5cfa0e120d49631e2
Adriano Costa <adrianokosta001@gmail.com>
2020-09-07 08:39:15 -0300

    Add lazy loading option to Image component

--- src/components/ui/Image/index.js
+++ src/components/ui/Image/index.js
@@ -1,6 +1,37 @@
+import React from 'react';
 import styled from 'styled-components';
+import IntersectionObserver from '@components/IntersectionObserver';
 
-const Image = styled.img({
+const Image = ({ lazy, placeholderImage, ...props }) =>
+  lazy
+    ? <LazyImage placeholderImage={placeholderImage} {...props} />
+    : <StyledImage {...props} />;
+
+const LazyImage = ({ placeholderImage, src, ...props }) => {
+  const [image, setImage] = React.useState({
+    fetched: false,
+    src: placeholderImage,
+  });
+
+  const handleImageIntersection = () => {
+    if (!image.fetched) {
+      setImage({
+        fetched: true,
+        src
+      })
+    }
+  }
+
+  return (
+    <IntersectionObserver
+      handleIntersection={handleImageIntersection}
+    >
+      <StyledImage src={image.src} {...props} />
+    </IntersectionObserver>
+  )
+}
+
+const StyledImage = styled.img({
   display: 'block'
 });
 
--- src/pages/Home/components/PokemonImage/index.js
+++ src/pages/Home/components/PokemonImage/index.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import Image from '@components/ui/Image';
+import questionMark from '@assets/images/question-mark.png';
 
 const PokemonImage = ({ imageUrl }) =>
-  <Image src={imageUrl} width="100%"/>
+  <Image
+    src={imageUrl}
+    lazy
+    placeholderImage={questionMark}
+    width="100%"
+  />
 
 export default PokemonImage;
